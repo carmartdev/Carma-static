7238398cc7df2b92a681cbfa0011f1ec0b6fa579
Carmartdev <amir8553mohammad@gmail.com>
2023-06-21 19:19:57 +0430

    Created carmastatic.js and app.js
    app.js is only for testing

---
+++ app.js
@@ -0,0 +1,6 @@
+email = "amirmohammad7@gmail.com"
+linkedinacount = "https://linkedin.com"
+footer_description = "Lorem Ipsum"
+addPostTable('Hello', posts)
+addPostTable("Wa'gw'an", posts)
+addPostTable('<h2 >Some of my <b>Newest Works</b></h2>', posts)
---
+++ carmastatic.js
@@ -0,0 +1,49 @@
+var footer_contact = document.getElementById("footer-contact")
+var post_table_div = document.getElementById("post-table")
+var admins_count = 1
+var linkedinacount = null
+var email = null
+var footer_description = null
+var post_tables = ['test1', 'test2']
+var posts = []
+
+function addPostTable(title, posts) {
+    post_tables.push(title)
+}
+function setEmail(youremail) {
+    email = youremail
+}
+for(var i=1; i< post_tables.length; i++){
+    console.log(post_tables)
+}
+
+
+
+
+var update_index = 0
+function update(){
+    if (admins_count <= 1){
+        footer_contact.innerHTML = "<div class='w3-row'><h2 class='w3-wide'>Contact Me</h2>"+ footer_description +"<p>Email: "
+        if(email != null){
+            footer_contact.innerHTML = footer_contact.innerHTML + "<b>"+ email +"</b></p>"
+        }
+        if (linkedinacount != null) {
+            footer_contact.innerHTML = footer_contact.innerHTML + "<a href='"+ linkedinacount +"'><img src='' alt='Linkedin'></a></div>"
+        }
+    }else if(admins_count >= 1){
+        footer_contact.innerHTML = "<div class='w3-row'><h2 class='w3-wide'>Contact Us</h2><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio aspernatur et nihil explicabo dolore aliquam a veniam assumenda voluptatibus, ut dolorem accusantium excepturi earum reprehenderit provident, est nesciunt. Laboriosam.</p><p>Email: <b>amirmohammadmehdipour7@gmail.com</b></p>"
+        if(email != null){
+            footer_contact.innerHTML = footer_contact.innerHTML + "<b>"+ email +"</b></p>"
+        }
+        if (linkedinacount != null) {
+            footer_contact.innerHTML = footer_contact.innerHTML + "<a href='"+ linkedinacount +"'><img src='' alt='Linkedin'></a></div>"
+        }
+    }
+    update_index += 1
+    if(update_index <= 1){
+        setTimeout(update, 1)
+    }else{
+        setTimeout(update, 3000)
+    }
+}
+update()
