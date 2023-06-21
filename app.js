f38ce4181f2869a8b0b9bc5d0b5fed8eab8a0f9a
Carmartdev <amir8553mohammad@gmail.com>
2023-06-21 20:01:15 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 email = "amirmohammadmehdipour7@gmail.com"
 linkedinacount = "https://linkedin.com"
-footer_description = "Lorem Ipsum"
+footer_description = "2023 Carmartdev all rights reserved"
 addPostTable('Hello', posts)
 addPostTable("Wa'gw'an", posts)
 addPostTable('<h2 >Some of my <b>Newest Works</b></h2>', posts)
